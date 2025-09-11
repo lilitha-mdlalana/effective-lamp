49578f47da8cb85ac8591e18ccf8e315cbf1410f
innomdlalana <innomdlalana@gmail.com>
2025-09-11 22:22:19 +0200

    Fix: fix output bug

--- week_1/hello_world.c
+++ week_1/hello_world.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
+int main(void){ 
     char *name = get_string("What is your name? ");
     if(name == NULL){
         fprintf(stderr, "Error reading input.\n");
     }
-    printf("Hello, ", name);
-    //todo:fix bug
+    printf("Hello, %s\n", name);
+    
 
     free(name);
     return 0;
